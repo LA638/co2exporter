308f76679312c0e4e6936e3f8664926db68ddc29
LA638 <4388060@gmail.com>
2022-09-06 11:36:44 +0300

    Removed temperature rounding and averaging

--- co2exporter.py
+++ co2exporter.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Prometheus http:8057 exporter for CO2|temperature monitoring with USB CO2 monitor
-from co2meter import CO2monitor
-from prometheus_client import Gauge, start_http_server
+# Prometheus http:8057 exporter for CO₂/temperature monitoring with USB CO₂ monitor
+
 from sys import stdout
 from time import sleep
+from co2meter import CO2monitor
+from prometheus_client import Gauge, start_http_server
 
-exporter_port = 8057
-request_delay = 10
-avg_count = 4
+
+EXPORTER_PORT = 8057
+REQUEST_DELAY = 10
+AVG_COUNT = 6
+
 
 # Creating metrics
-MONITOR_REQUEST_DURATION_SECONDS = Gauge(
-    'monitor_request_duration_seconds', 'USB CO2 Monitor Request Duration')
-TEMPERATURE_C = Gauge('temperature_c', 'Current Temperature, C')
-CO2_PPM = Gauge('co2_ppm', 'CO2 Level, PPM')
+MONITOR_REQUEST_DURATION_SECONDS = Gauge('monitor_request_duration_seconds', 
+                                         'USB CO2 Monitor Request Duration')
+TEMPERATURE_C = Gauge('temperature_c', 
+                      'Current Temperature, C')
+CO2_PPM = Gauge('co2_ppm', 
+                'CO2 Level, PPM')
+
 
 # Decorate function with metric to measure duration of request
 @MONITOR_REQUEST_DURATION_SECONDS.time()
@@ -23,13 +29,16 @@ def get_data(monitor):
     try:
         return(monitor.read_data())
     except Exception as error:
-        return(error)
+        stdout.write(str(error))
+        stdout.flush()
+        sleep(5)
 
 
 if __name__ == '__main__':
 
     # Start up the server to expose the metrics
-    start_http_server(exporter_port)
+    start_http_server(EXPORTER_PORT)
+
     # Init the monitor
     try:
         mon = CO2monitor(bypass_decrypt=True)
@@ -39,40 +48,19 @@ if __name__ == '__main__':
         sleep(5)
         exit(1)
 
-    data = get_data(mon)
-    CO2_PPM.set(data[1])
-    old_temp = int(round(data[2]*10, 0))
-    TEMPERATURE_C.set(old_temp/10)
-
-    temp_count = 0
-
-    # Request data
     while True:
+        # Request data from monitor
         data = get_data(mon)
-        CO2_PPM.set(data[1])
-        curr_temp = int(round(data[2]*10000, 0))/10000
-        TEMPERATURE_C.set(curr_temp)
-
-        # Temperature averaging
-        # if curr_temp == old_temp:
-        #     temp_count = 0
-        #     stdout.write(f'{temp_count} {old_temp} {curr_temp} No changes\n')
-        #     stdout.flush()
-        # else:
-        #     temp_count += 1
-        #     if temp_count == avg_count:
-        #         TEMPERATURE_C.set(curr_temp/10)
-        #         stdout.write(f'{temp_count} {old_temp} {curr_temp} Update\n')
-        #         stdout.flush()
-        #         old_temp = curr_temp
-        #         temp_count = 0
-        #     else:
-        #         stdout.write(f'{temp_count} {old_temp} {curr_temp} Changed\n')
-        #         stdout.flush()
-
-        last_request_duration = MONITOR_REQUEST_DURATION_SECONDS._value.get()
         
-        stdout.write(f'{data[1]}; {curr_temp}; {round(last_request_duration, 2)}\n')
+        # Set metrics
+        CO2_PPM.set(data[1])
+        TEMPERATURE_C.set(data[2])
+        
+        # Calculate next delay
+        last_request_duration = round(MONITOR_REQUEST_DURATION_SECONDS._value.get(), 2)
+        delay = REQUEST_DELAY - last_request_duration
 
-        delay = request_delay - last_request_duration
+        # Write to systemd unit log
+        stdout.write(f'{data[1]}; {data[2]}; {last_request_duration}\n')
+
         sleep(delay)
